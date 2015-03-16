44d98c116f9ff65bd552f63964ab04165ec4bd2a
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-16 17:17:27 +0300

    add representation

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -1,37 +1,39 @@
 from checkio_referee import RefereeCodeGolf
-from checkio_referee.covercode import py_unwrap_args
-from checkio_referee.validators import BaseValidator, ValidatorResult
+from checkio_referee import covercodes, validators, representations
 
 import settings
 import settings_env
 from tests import TESTS
 
 
-class AutoPaintingValidator(BaseValidator):
+class AutoPaintingValidator(validators.BaseValidator):
     def validate(self, outer_result):
         steps, k, n = self._test["validation_data"]
         if not isinstance(outer_result, str):
-            return ValidatorResult(False, "This is not a string.")
+            return validators.ValidatorResult(False, "This is not a string.")
         actions = outer_result.split(",")
         if len(actions) > steps:
-            return ValidatorResult(False, "It can be shorter.")
+            return validators.ValidatorResult(False, "It can be shorter.")
         details = [0 for _ in range(n)]
         good_ch = "".join(str(r) for r in range(n))
         good_ch += ","
         if any(ch not in good_ch for ch in outer_result):
-            return ValidatorResult(False, "Wrong symbol in the result.")
+            return validators.ValidatorResult(False, "Wrong symbol in the result.")
         for act in actions:
             if len(act) > k:
-                return ValidatorResult(False, "The system can contain {0} detail(s).".format(k))
+                return validators.ValidatorResult(
+                    False, "The system can contain {0} detail(s).".format(k))
             if len(set(act)) < len(act):
-                return ValidatorResult(False, "You can not place one detail twice in one load")
+                return validators.ValidatorResult(
+                    False, "You can not place one detail twice in one load")
             for ch in act:
                 details[int(ch)] += 1
         if any(d < 2 for d in details):
-            return ValidatorResult(False, "I see no painted details.")
+            return validators.ValidatorResult(False, "I see no painted details.")
         if any(d > 2 for d in details):
-            return ValidatorResult(False, "I see over painted details.")
-        return ValidatorResult(True)
+            return validators.ValidatorResult(False, "I see over painted details.")
+        return validators.ValidatorResult(True)
+
 
 class Referee(RefereeCodeGolf):
     TESTS = TESTS
@@ -41,8 +43,13 @@ class Referee(RefereeCodeGolf):
     CURRENT_ENV = settings_env.CURRENT_ENV
     FUNCTION_NAME = "golf"
     VALIDATOR = AutoPaintingValidator
+    CALLED_REPRESENTATIONS = {
+        "python_3": representations.unwrap_arg_representation,
+        "python_2": representations.unwrap_arg_representation,
+        "javascript": representations.unwrap_arg_representation
+    }
     ENV_COVERCODE = {
-        "python_2": py_unwrap_args,
-        "python_3": py_unwrap_args,
-        "javascript": None
+        "python_2": covercodes.py_unwrap_args,
+        "python_3": covercodes.py_unwrap_args,
+        "javascript": covercodes.py_unwrap_args
     }
